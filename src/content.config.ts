529b66dd15153cb4af5478138eecb2f4a6a09bac
johannroux <johann.ej.roux@gmail.com>
2026-05-08 10:59:09 +0200

    fix: Split z import into astro/zod to fix lint warning

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 
 const absoluteOrSitePathUrl = z.string().refine((value) => value.startsWith("/") || /^https?:\/\//i.test(value), {
